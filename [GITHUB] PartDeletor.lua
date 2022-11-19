ea7be70c9b96bde4e4243b37e3597f923e0a7342
kreekman <118077319+kreekman@users.noreply.github.com>
2022-11-19 12:30:02 -0500

    Update [GITHUB] PartDeletor.lua

--- [GITHUB] PartDeletor.lua
+++ [GITHUB] PartDeletor.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kreekman/Part-Deletor/main/RAW%20PartDeletor.lua"))()
+-- re-instal the script if it doesn't work from the GitHub: https://github.com/kreekman/Part-Deletor

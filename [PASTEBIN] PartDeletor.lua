840819d938813651ec72e3beda48ba3ae80466f9
kreekman <118077319+kreekman@users.noreply.github.com>
2022-11-19 12:40:52 -0500

    Added pcall

--- [PASTEBIN] PartDeletor.lua
+++ [PASTEBIN] PartDeletor.lua
@@ -1,2 +1,8 @@
 loadstring(game:HttpGet("https://pastebin.com/raw/sthXfBdm"))()
 --re-install the script if it doesn't work from the GitHub: https://github.com/kreekman/Part-Deletor
+
+
+if not success then
+	warn([[Error loading: ]]..errormessage..[[ 
+		Try re-installing the scripts at https://github.com/kreekman/Part-Deletor]])
+end
